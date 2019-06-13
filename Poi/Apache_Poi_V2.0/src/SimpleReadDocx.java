1c608a2f5da0c1bb961acfa66ebe262078402041
“Your <sangmukcho.tsis@gmail.com>
2019-06-13 16:41:51 +0900

    add Poi_V2.0

---
+++ Poi/Apache_Poi_V2.0/src/SimpleReadDocx.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class SimpleReadDocx {
+
+	public static void main(String[] args) {
+		try {
+
+			File target = new File("doc/test.xml");
+			
+			FileInputStream fis = new FileInputStream(target);
+			OPCPackage opc = null;
+			try {
+				opc = OPCPackage.open(fis);
+				
+			} catch (InvalidFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("\n\n\n------------------------------------\n\n\n");
+			XWPFDocument docx = new XWPFDocument(opc);
+			System.out.println(docx.getDocument()+"\n");
+			
+			
+			XWPFWordExtractor we = new XWPFWordExtractor(docx);
+			
+			
+			System.out.println(we.getText());
+			
+			we.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Poi/Apache_Poi_V2.0/src/Test2.java
@@ -0,0 +1,92 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		long time1 = System.currentTimeMillis ();
+		try {
+			File file = new File("doc/test.txt");
+			FileInputStream fis = new FileInputStream(file);
+			int ch;
+
+			while (fis.available() > 0) {
+				ch =  fis.read();
+				System.out.print((char)ch);
+			}
+			
+			fis.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		long time2 = System.currentTimeMillis ();
+		System.out.println("FileInputStream : "+ (time2-time1)+"\n\n\n\n");
+	
+
+		time1 = System.currentTimeMillis ();
+		try {
+			File file = new File("doc/test.txt");
+            FileReader filereader = new FileReader(file);
+			int ch = 0;
+
+			while (ch != -1) {
+				ch =  filereader.read();
+				System.out.print((char)ch);
+			}
+			
+			filereader.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		time2 = System.currentTimeMillis ();
+		System.out.println("filereader : "+ (time2-time1)+"\n\n\n\n");
+		
+		
+		
+		time1 = System.currentTimeMillis ();
+		try{
+            File file = new File("doc/test.txt");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+                System.out.println(line);
+            }         
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+        }catch(IOException e){
+            System.out.println(e);
+        }
+		time2 = System.currentTimeMillis ();
+		System.out.println("BufferedReader.readLine : "+ (time2-time1)+"\n\n\n\n");
+		
+		time1 = System.currentTimeMillis ();
+		try{
+            File file = new File("doc/test.txt");
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            int ch;
+            while((ch = bufReader.read()) != -1){
+                System.out.print((char) ch);
+            }         
+            bufReader.close();
+        }catch (FileNotFoundException e) {
+        }catch(IOException e){
+            System.out.println(e);
+        }
+		time2 = System.currentTimeMillis ();
+		System.out.println("BufferedReader.read : "+ (time2-time1)+"\n\n\n\n");
+		
+	}
+	
+	
+
+}
