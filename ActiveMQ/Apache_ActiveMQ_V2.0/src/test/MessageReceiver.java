89ea21a07d6af6e57bddd8801cbc894d3b79289a
tkdanr612 <whtkdanr@naver.com>
2019-06-22 11:56:44 +0900

    add ActiveMQ_V2.0

---
+++ ActiveMQ/Apache_ActiveMQ_V2.0/src/test/MessageReceiver.java
@@ -0,0 +1,48 @@
+package test;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class MessageReceiver {
+
+	// default URL을 사용하게된다. 여기서는 localhost:8161
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+	// 메세지 큐의 이름을 설정하며 어떠한 이름도 사용가능하다.
+	private static String subject = "Test";
+
+	public static void main(String[] args) throws JMSException {
+		// ActiveMQ 와 연결하는 ConnectionFactory,Connection 을 생성하고 시작한다.
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		// session을 생성한다.
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		// 목적지는 새로 생성된 "Test"라는 Queue가 된다.
+		Destination destination = session.createQueue(subject);
+
+		// MessageConsumer 즉, receive를 만든다.
+		MessageConsumer consumer = session.createConsumer(destination);
+
+		// 메세지를 받아온다.
+		Message message = consumer.receive();
+
+		// message 가 TextMessage 객체라면 .getText() 메소드를 통해 해당 메세지를 출력한다.
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			System.out.println("Received message '" + textMessage.getText() + "'");
+		}
+		connection.close();
+	}
+}
---
+++ ActiveMQ/Apache_ActiveMQ_V2.0/src/test/MessageSender.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.time.LocalDateTime;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class MessageSender {
+
+	// default URL을 사용하게된다. 여기서는 localhost:8161
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+	// 메세지 큐의 이름을 설정하며 어떠한 이름도 사용가능하다.
+	private static String subject = "Test";
+
+	public static void main(String[] args) throws JMSException {
+		// ActiveMQ 와 연결하는 ConnectionFactory,Connection 을 생성하고 시작한다.
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		// session을 생성한다.
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		// 목적지는 새로 생성된 "Test"라는 Queue가 된다.
+		Destination destination = session.createQueue(subject);
+
+		// 메세지를 보내는 역을 한다.
+		MessageProducer producer = session.createProducer(destination);
+
+		// server로 보낼 메세지를 설정하고 메세지를 전송한다.
+		for (int i = 0; i < 10; i++) {
+			LocalDateTime nowTime = LocalDateTime.now();
+			TextMessage message = session
+					.createTextMessage("Hello !!! Welcome to the world of ActiveMQ.\n{" + nowTime + "}");
+
+			producer.send(message);
+			System.out.println("JCG printing@@ '" + message.getText() + "'");
+		}
+
+		connection.close();
+	}
+}
